a7210c2496188879373d13952deb763b7b9d8554
Yagna Sri Vintha <w10125181@usm.edu>
2021-11-30 13:45:12 -0800

    Add project files.

---
+++ TempConverter/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace TempConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(481, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(49, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 101);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Starting Temperature : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(366, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(276, 32);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 310);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ TempConverter/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = "C";
+            //comboBox1.Items.Add("C");
+            comboBox1.Items.Add("F");
+            comboBox1.Items.Add("K");
+            pictureBox1.Image = Properties.Resources.temperaturegauge_none;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            string Fahrenheitvalue = "";
+            try
+            {
+                if (!string.IsNullOrEmpty(comboBox1.Text))
+                {
+                    if (comboBox1.Text == "C")
+                    {
+                        decimal selectedval = numericUpDown1.Value;
+                        Fahrenheitvalue = ((selectedval * 9 / 5) + 32).ToString();
+                        label2.Text = Fahrenheitvalue + "F";
+                        label3.Text = (selectedval + (Decimal)273.15).ToString("0.00") + "K";
+
+                    }
+
+                    else if (comboBox1.Text == "F")
+                    {
+                        decimal selectedval = numericUpDown1.Value;
+                        Fahrenheitvalue = numericUpDown1.Value.ToString();
+                        label2.Text = ((selectedval - 32) * 5 / 9).ToString("0.00") + "°C";
+                        label3.Text = (((selectedval - 32) * 5 / 9) + (Decimal)273.15).ToString("0.00") + "K";
+                    }
+
+                    else if (comboBox1.Text == "K")
+                    {
+                        decimal selectedval = numericUpDown1.Value;
+                        Fahrenheitvalue = ((selectedval - (Decimal)273.15) * 9 / 5 + 32).ToString();
+                        label2.Text = (selectedval - (Decimal)273.15).ToString("0.00") + "°C";
+                        label3.Text = Fahrenheitvalue + "F";
+
+                    }
+                    if (!string.IsNullOrWhiteSpace(Fahrenheitvalue))
+                    {
+                        decimal result = decimal.Parse(Fahrenheitvalue);
+                        if (result > 95)
+                            pictureBox1.Image = Properties.Resources.temperaturegauge_hot;
+                        if (75 < result && result < 95)
+                            pictureBox1.Image = Properties.Resources.temperaturegauge_warm;
+                        if (55 < result && result < 75)
+                            pictureBox1.Image = Properties.Resources.temperaturegauge_mild;
+                        if (33 < result && result < 55)
+                            pictureBox1.Image = Properties.Resources.temperaturegauge_cold;
+                        if (result < 33)
+                            pictureBox1.Image = Properties.Resources.temperaturegauge_warm;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                
+                MessageBox.Show("value cant be 0");
+            }
+        }
+    }
+}
